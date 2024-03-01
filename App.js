53ff77b35c93219f65fa7e6a052cee7496146a31
ShawnFrost26 <sb262202@gmail.com>
2024-03-01 12:53:16 +0530

    Functional Component

--- App.js
+++ App.js
@@ -4,6 +4,13 @@ import ReactDOM from "react-dom/client";
 //JSX
 const jsxHeading = <h1 className="head">Namaste React using JSX</h1>;
 
+//Functional component
+const HeadingComponent = () => (
+  <div id="container">
+    <h1 className="heading">Namaste React from Functional Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
