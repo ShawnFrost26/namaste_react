39587681c988025eaf47e05435ba5602dd290726
ShawnFrost26 <sb262202@gmail.com>
2024-03-01 12:32:02 +0530

    removed react element code

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
-
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-
 //JSX
 const jsxHeading = <h1 className="head">Namaste React using JSX</h1>;
 
