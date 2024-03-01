82aad0c4e9ca73a8e9ae4be86a3afe78dd5f9dc9
ShawnFrost26 <sb262202@gmail.com>
2024-03-01 11:46:44 +0530

    React Element vs JSX

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const nestedElements = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Nested elements from React!!"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Nested elements from React!!"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-  ])
-);
-console.log(nestedElements);
+//React Element
+
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+
+//JSX
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(nestedElements);
+
+root.render(jsxHeading);
