94030d6ddd2bd1bea606a874c039adb76751fa97
ShawnFrost26 <sb262202@gmail.com>
2024-03-01 17:22:31 +0530

    added components

--- App.js
+++ App.js
@@ -1,16 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//JSX
-const jsxHeading = <h1 className="head">Namaste React using JSX</h1>;
-
-//Functional component
-const HeadingComponent = () => (
-  <div id="container">
-    <h1 className="heading">Namaste React from Functional Component</h1>
-  </div>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <h1 className="logo">ReactEats</h1>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/x6x1srh1d4mlnyppoyvw"
+        alt="res-logo"
+      />
+      <h3>Meghana Foods</h3>
+      <h4>Biryani, North Indian</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+        <input type="search" placeholder="Search here" />
+      </div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
