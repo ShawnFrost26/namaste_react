dfdcb0ebe5b1103d65d20364cfa5a0015ef76353
ShawnFrost26 <sb262202@gmail.com>
2024-03-04 17:38:11 +0530

    added useState hook

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
 import resObj from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRes, setListOfRes] = useState(resObj);
+
   return (
     <div className="body">
       <div className="search">
         <input type="search" placeholder="Search here" />
       </div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            filteredList = listOfRes.filter((res) => res.info.avgRating > 4.5);
+            setListOfRes(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resObj.map((restaurant) => (
+        {listOfRes.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} restaurant={restaurant} />
         ))}
       </div>
