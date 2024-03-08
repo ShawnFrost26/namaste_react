789551269c0ef8947a3cbd90c1d022f572293cc3
ShawnFrost26 <sb262202@gmail.com>
2024-03-08 10:28:02 +0530

    implemented searchbar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -4,20 +4,41 @@ import { useState } from "react";
 
 const Body = () => {
   const [listOfRes, setListOfRes] = useState(resObj);
+  const [searchWord, setSearchWord] = useState("");
+
+  const handleTopRatedRes = () => {
+    filteredList = listOfRes.filter((res) => res.info.avgRating > 4.5);
+    setListOfRes(filteredList);
+  };
+
+  const handleSearch = (e) => {
+    const inputText = e.target.value;
+    setSearchWord(inputText);
+
+    if (!inputText || inputText.trim() == "") {
+      // If the search bar is empty, reset the list to the original data
+      setListOfRes(resObj);
+    } else {
+      const searchList = resObj.filter((res) =>
+        res.info.name.toLowerCase().includes(inputText.toLowerCase())
+      );
+      setListOfRes(searchList);
+    }
+  };
 
   return (
     <div className="body">
       <div className="search">
-        <input type="search" placeholder="Search here" />
+        <input
+          type="search"
+          placeholder="Search here"
+          value={searchWord}
+          onChange={handleSearch}
+        />
+        {/* <button onClick={handleSearch}>Search</button> */}
       </div>
       <div className="filter">
-        <button
-          className="filter-btn"
-          onClick={() => {
-            filteredList = listOfRes.filter((res) => res.info.avgRating > 4.5);
-            setListOfRes(filteredList);
-          }}
-        >
+        <button className="filter-btn" onClick={handleTopRatedRes}>
           Top Rated Restaurants
         </button>
       </div>
